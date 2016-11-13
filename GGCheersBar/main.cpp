6fa29c80ea673600a7b4d2cb69af107b62338b9a
ZackLYChen <p19745h@gmail.com>
2016-11-13 23:48:42 +0800

    new socket

--- GGCheersBar/main.cpp
+++ GGCheersBar/main.cpp
@@ -1,13 +1,54 @@
+#pragma comment(lib, "ws2_32.lib") 
 #define _CRT_SECURE_NO_WARNINGS
 #include <cstdio>
+#include <string>
 #include <chrono>
 #include "go.h"
 #include "mcts.h"
+#include <winsock2.h>
 using namespace std::chrono;
 using namespace GGCheersBar;
+#define MAX_SIZE 2048
 
 Position readPosition();
 int main() {
+	/****Socket****/
+	SOCKET serverSocket, clientSocket;
+	struct sockaddr_in serverAddress, clientAddress;
+	int clientAddressLen;
+	int bytesRead;
+	char buf[MAX_SIZE], instr[MAX_SIZE], instr_cplmt[MAX_SIZE];
+	int SERVER_PORT;
+	WSADATA wsadata;
+	if (WSAStartup(MAKEWORD(2, 2), (LPWSADATA)&wsadata) != 0) {
+		// MY_ERROR("Winsock Error\n");
+	}
+	char name[50];
+	serverSocket = socket(PF_INET, SOCK_STREAM, 0);
+	while (1) {
+		gets_s(instr);
+		scanf(instr, "%s [%d]", name, &SERVER_PORT);
+		std::string inst(name);
+		if (inst == "begin") {
+			break;
+		}
+		else {
+			printf("Input is error.\n");
+		}
+	}
+	memset(&serverAddress, 0, sizeof(serverAddress));
+	serverAddress.sin_family = AF_INET;
+	serverAddress.sin_addr.s_addr = htonl(INADDR_ANY);
+	serverAddress.sin_port = htons(SERVER_PORT);
+
+	if (bind(serverSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) < 0) {
+		// MY_ERROR("Bind Error\n");
+	}
+
+	if (listen(serverSocket, 3) < 0) {
+		// MY_ERROR("Listen Error\n");
+	}
+	/*************/
 	Go go(Black, Position(8, 7));
 	Chess AI;
 	State judge;
